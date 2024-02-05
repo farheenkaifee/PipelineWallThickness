c907aee9018e1df3f2440dc8055b9f94091fec83
Farheen Kaifee <farheenkaifee555@gmail.com>
2024-02-05 10:29:40 +0530

    Reset function is added to the main file

--- Features/Reset.py
+++
@@ -1,7 +0,0 @@
-
-
-def reset_all(self,Outside_Diameter_OD, Nominal_Wall_Thickness_tnom, Fabrication_Thickness_Tolerance_tfab):
-            #   Corrosion_Allowance_tcorr, Ovality_of_Pipe_Oo, SMYS_σsmys, SMTS_σsmts, Derating_value_temp_yieldStress_fy_temp, Derating_value_temp_tensileStress_fu_temp, Youngs_Modulus_E, Poission_s_Ratio_ν, Maximum_Fabrication_Factor_alpha_fab, Material_Strength_Factor_alpha_u, Pd, Material_resistant_factor_gamma_m, Pmin, Elevation_at_Pressure_Reference_Level_href, Elevation_level_at_Pressure_Point_hl, Product_Density_ρcont, Hydrotest_Water_Density_ρt, Incidental_to_Design_Pressure_Ratio_gamma_inc, Water_Depth_WD, Sea_Water_Density_ρsea, Max_Elevation_wrt_MSL_hmax, Min_Elevation_wrt_MSL_hmin, Safety_Class_RF_gamma_SCPC, Safety_Class_RF_gamma_SCLB, System_Pressure_Test_Factor_alpha_spt, Mill_Pressure_Test_Factor_alpha_mpt):
-    self.Outside_Diameter_OD.clear()
-    self.Nominal_Wall_Thickness_tnom.clear()
-    self.Fabrication_Thickness_Tolerance_tfab.clear()
