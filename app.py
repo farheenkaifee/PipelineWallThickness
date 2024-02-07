847fe002e2bfaabaa19bcd63086f3efb8a53771c
Farheen Kaifee <farheenkaifee555@gmail.com>
2024-02-07 14:44:39 +0530

    Result label is added

--- app.py
+++ app.py
@@ -1020,6 +1020,8 @@ class Ui_MainWindow(object):
 
                     self.treq_lineedit.setText(Nominal_Wall_Thickness_tnom)
 
+                    # print(systemTest_List)
+
 
                 elif( j==2):
                     UC_prss_cont,P_check= pressure_operation(Outside_Diameter_OD, Nominal_Wall_Thickness_tnom, Fabrication_Thickness_Tolerance_tfab, Corrosion_Allowance_tcorr, Ovality_of_Pipe_Oo,SMYS_σsmys,SMTS_σsmts,Derating_value_temp_yieldStress_fy_temp,Derating_value_temp_tensileStress_fu_temp,Youngs_Modulus_E ,Poission_s_Ratio_ν ,Maximum_Fabrication_Factor_alpha_fab,Pd,Pmin,Elevation_at_Pressure_Reference_Level_href,Elevation_level_at_Pressure_Point_hl ,Product_Density_ρcont,Hydrotest_Water_Density_ρt,Incidental_to_Design_Pressure_Ratio_gamma_inc,Water_Depth_WD,Sea_Water_Density_ρsea,Min_Elevation_wrt_MSL_hmin,Safety_Class_RF_gamma_SCPC,Mill_Pressure_Test_Factor_alpha_mpt,Material_Strength_Factor_alpha_u,Material_resistant_factor_gamma_m,System_Pressure_Test_Factor_alpha_spt)
