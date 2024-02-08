b6fdb6757761a813cc20f4779e434f99e54dbcad
superman1408 <superman14082017@gmail.com>
2024-02-08 17:38:20 +0530

    Reporting function.....git add --all!

--- Features/Report.py
+++ Features/Report.py
@@ -1,9 +1,38 @@
 import csv
 import pandas as pd
 from tkinter import filedialog
+import random
+from PyQt5.QtWidgets import QMessageBox
 
 
 
 
 def report(variable):
     print(f"Hello there your report is being prepared..!!!\n\t\t{variable}")
+    try:
+                
+        filepath = filedialog.asksaveasfilename(
+        # os.getenv('home'),
+                initialdir= "C:/Users/DELL/Desktop",
+                title= "Save As",
+                defaultextension= "*.csv",
+                filetypes=(
+                        ("csv file","*.csv"),
+                        ("HTML file","*.html"),
+                        ("text file","*.txt"),
+                        ("All file","*.*")
+                )
+        )
+        print(filepath)
+    except:
+        print(f"error code:{random.random()}>>>>>>Error in saving file while opening to write..$$%%^^&&&")
+    try:
+        if filepath is None:
+                return
+        else:
+            with open(filepath, 'w'):
+                    df = pd.DataFrame({"Values": variable})
+                    df.to_csv(filepath)
+    except:
+        print(f"error code:{random.random()}>>>>>>ERROR in saving your file...!!")
+        QMessageBox.warning(None, "Warning", 'File is not saved..!!!!')
