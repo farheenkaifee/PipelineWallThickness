f838d16f5159786654bd57d38ec073d3e37bde1d
Farheen Kaifee <farheenkaifee555@gmail.com>
2024-02-08 17:55:39 +0530

    modified

--- app.py
+++ app.py
@@ -914,16 +914,13 @@ class Ui_MainWindow(object):
         self.actionSave_As.triggered.connect(self.saveAs_doc)
         self.actionReset.triggered.connect(self.reset)
         self.actionOpen.triggered.connect(self.open_Doc)
-<<<<<<< HEAD
 
         self.actionDocumentation.triggered.connect(self.document)
-=======
         self.menuViewReport.triggered.connect(self.create_report)
         
         
 # ..............................Some Global variables..................................................................................
         self.list_Variable = ["**......hello my name is list......***"]
->>>>>>> 5e881c98f0cf228e1c38b85c40d016d007a6bf46
         
         
 #................................ ALL LINKS  ARE HERE <------------------------------->
@@ -1049,11 +1046,7 @@ class Ui_MainWindow(object):
 
 
                     elif( j==2):
-<<<<<<< HEAD
-                        UC_prss_cont,P_check = pressure_operation(Outside_Diameter_OD, Nominal_Wall_Thickness_tnom, Fabrication_Thickness_Tolerance_tfab, Corrosion_Allowance_tcorr, Ovality_of_Pipe_Oo,SMYS_σsmys,SMTS_σsmts,Derating_value_temp_yieldStress_fy_temp,Derating_value_temp_tensileStress_fu_temp,Youngs_Modulus_E ,Poission_s_Ratio_ν ,Maximum_Fabrication_Factor_alpha_fab,Pd,Elevation_at_Pressure_Reference_Level_href,Elevation_level_at_Pressure_Point_hl ,Product_Density_ρcont,Hydrotest_Water_Density_ρt,Incidental_to_Design_Pressure_Ratio_gamma_inc,Water_Depth_WD,Sea_Water_Density_ρsea,Min_Elevation_wrt_MSL_hmin,Safety_Class_RF_gamma_SCPC,Mill_Pressure_Test_Factor_alpha_mpt,Material_Strength_Factor_alpha_u,Material_resistant_factor_gamma_m,System_Pressure_Test_Factor_alpha_spt)
-=======
                         UC_prss_cont,P_check, self.list_Variable = pressure_operation(Outside_Diameter_OD, Nominal_Wall_Thickness_tnom, Fabrication_Thickness_Tolerance_tfab, Corrosion_Allowance_tcorr, Ovality_of_Pipe_Oo,SMYS_σsmys,SMTS_σsmts,Derating_value_temp_yieldStress_fy_temp,Derating_value_temp_tensileStress_fu_temp,Youngs_Modulus_E ,Poission_s_Ratio_ν ,Maximum_Fabrication_Factor_alpha_fab,Pd,Pmin,Elevation_at_Pressure_Reference_Level_href,Elevation_level_at_Pressure_Point_hl ,Product_Density_ρcont,Hydrotest_Water_Density_ρt,Incidental_to_Design_Pressure_Ratio_gamma_inc,Water_Depth_WD,Sea_Water_Density_ρsea,Min_Elevation_wrt_MSL_hmin,Safety_Class_RF_gamma_SCPC,Mill_Pressure_Test_Factor_alpha_mpt,Material_Strength_Factor_alpha_u,Material_resistant_factor_gamma_m,System_Pressure_Test_Factor_alpha_spt)
->>>>>>> 5e881c98f0cf228e1c38b85c40d016d007a6bf46
                         # print(value_pressure_operation)
                         print(self.list_Variable)
 
